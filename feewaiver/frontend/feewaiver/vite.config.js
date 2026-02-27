44061c8ef26c2fdfd3c12dd3ea4cfda8806b1cb5
shibaken <katsufumi.shibata@dbca.wa.gov.au>
2026-02-27 09:07:19 +0800

    Fix vite.config.js: remove invalid eslint minErrorLevel option

--- feewaiver/frontend/feewaiver/vite.config.js
+++ feewaiver/frontend/feewaiver/vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig(({ command }) => ({
     },
     plugins: [
         vue(),
-        eslint({ minErrorLevel: 'error' }),
+        eslint(),
         svgLoader({
             defaultImport: 'url',
         }),
